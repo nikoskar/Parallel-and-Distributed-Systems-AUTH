b839de83f6e674999dbd6417691069618da8761f
nikoskar <nikoskarabe95@gmail.com>
2020-11-17 20:10:39 +0200

    v2 done

--- graph.c
+++ graph.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-#define NUM_NODES 4
+#define NUM_NODES 5
 
 int** buildGraph() {
 
@@ -18,20 +18,62 @@ int** buildGraph() {
 			else adj[i][j] = adj[j][i] = rand() % 2;	
 		}
 	}
-
+	
 	return adj;
 }
 
 int main(int argc, char *argv[]) {
-	
+
+	int c3[NUM_NODES] = {0};
 	int **arr = buildGraph();
+	long elapsed_sec, elapsed_nsec;
+	struct timespec ts_start, ts_end;
+	
+	printf("\n ########## ADJ MATRIX ##########\n");
 
 	for (int i = 0; i < NUM_NODES; i++) {
 		for (int j = 0; j < NUM_NODES; j++) {
-			printf("%d",arr[i][j]);
+			printf(" %d",arr[i][j]);
 		}
 		printf("\n");
 	}
+	printf (" ################################\n\n");
+
+	clock_gettime(CLOCK_MONOTONIC, &ts_start);
+
+	// START COUNTING THE TRIANGLES
+	for (int i = 0; i < NUM_NODES; i++) {
+		for (int j = 0; j < NUM_NODES; j++) {
+			if(i >= j) {
+				continue;
+			}
+			for (int k = 0; k < NUM_NODES; k++) {
+				if(j >= k) {
+					continue;
+				}
+				//if ( arr[i][j] == arr[j][k] == arr[k][i] == 1) {
+				if (arr[i][j] == arr[j][k] && arr[j][k] == arr[k][i] && arr[k][i] == 1) {
+					c3[i]++; 
+					c3[j]++; 
+					c3[k]++;
+				}
+			}
+		}
+	}	
 	
+	clock_gettime(CLOCK_MONOTONIC, &ts_end);
+	elapsed_sec = ts_end.tv_sec - ts_start.tv_sec;
+	elapsed_nsec = ts_end.tv_nsec - ts_start.tv_nsec;
+
+	printf("Elapsed time in seconds: %ld\n", elapsed_sec);
+	printf("Elapsed time in nanoseconds: %ld\n", elapsed_nsec);
+	printf (" \n################################\n\n");
+	
+	for (int i = 0; i < NUM_NODES; i++) {
+		printf(" Node %d has %d incident triangles\n", i, c3[i]);
+	}
+
+	printf (" \n################################\n\n");
+
 	free(arr);
 }
