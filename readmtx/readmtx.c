7738d62f3c64db2b8e3a0ecb449dc884192ff05c
nikoskar <nikoskarabe95@gmail.com>
2020-11-29 22:51:37 +0200

    serial

--- readmtx/readmtx.c
+++ readmtx/readmtx.c
@@ -5,19 +5,68 @@
 #include <time.h>
 #include "mmio.h"
 
-int return_index(int i, uint32_t *col, uint *row) {
+#define foreach(item, array) \
+    for(int keep = 1, \
+            count = 0,\
+            size = sizeof (array) / sizeof *(array); \
+        keep && count != size; \
+        keep = !keep, count++) \
+      for(item = (array) + count; keep; keep = !keep)
 
-  uint32_t start = col[i];
-  uint32_t end = col[i + 1];
 
-  for (int pos = start; pos < end; pos++) {
-    if (row[pos] <= i) {
-      continue;
-    } else {
-      return pos;
+int *printsliced (uint32_t start,
+                  uint32_t end,
+                  uint32_t i,
+                  uint32_t *csc_col,
+                  uint32_t *csc_row,
+                  uint32_t N) {
+
+
+  int size = end - start;
+  int *sliced = malloc(size*sizeof(int));
+  int *none = malloc(1*sizeof(int));
+  none[0] = 0;
+  int flag = 0;
+
+
+
+  for (int x = 0; x < size; x++) {
+    sliced[x] = 0;
+  }
+
+
+  int id = 0;
+  for(uint32_t x = start; x < end; x++) {
+    if(csc_row[x] > i && csc_row[x] < N-1)
+    {
+      sliced[id] = csc_row[x];
+      id++;
+      flag = 1;
     }
   }
+
+  int *final_sliced = malloc(size*sizeof(int));
+  int final_size = id;
+  for (int x = 0; x < size; x++) {
+    final_sliced[x] = 0;
+  }
+
+  for(int x = 0; x < id; x++) {
+    final_sliced[x] = sliced[x];
+  }
+  // printf("\nrow : %d, SLICED FINAL IS: ", i);
+  //
+  // for(int x = 0; x < final_size; x++) {
+  //   printf("%d, ", final_sliced[x]);
+  // }
+  if(flag) {
+    return final_sliced;
+  } else {
+    return none;
+  }
+
 }
+
 
 void coo2csc(
   uint32_t       * const row,       /*!< CSC row start indices */
@@ -91,25 +140,33 @@ int main(int argc, char *argv[])
         exit(1);
 
     /* reseve memory for matrices */
-    I = (uint32_t *) malloc(nz * sizeof(uint32_t));
-    J = (uint32_t *) malloc(nz * sizeof(uint32_t));
+    I = (uint32_t *) malloc(2*nz * sizeof(uint32_t));
+    J = (uint32_t *) malloc(2*nz * sizeof(uint32_t));
     val = (double *) malloc(nz * sizeof(double));
 
     if (!mm_is_pattern(matcode))
     {
-      for (i=0; i<nz; i++) {
+      for (i=0; i<2*nz; i++) {
           fscanf(f, "%d %d %lg\n", &I[i], &J[i], &val[i]);
+          I[nz+i] = J[i];
+          J[nz+i] = I[i];
           I[i]--;  /* adjust from 1-based to 0-based */
           J[i]--;
+          J[nz+i]--;
+          I[nz+i]--;
       }
     }
     else{
       for (i=0; i<nz; i++)
       {
           fscanf(f, "%d %d\n", &I[i], &J[i]);
+          I[nz+i] = J[i];
+          J[nz+i] = I[i];
           val[i]=1;
           I[i]--;  /* adjust from 1-based to 0-based */
           J[i]--;
+          J[nz+i]--;
+          I[nz+i]--;
       }
     }
 
@@ -120,21 +177,21 @@ int main(int argc, char *argv[])
 
 
     uint32_t isOneBased = 0;
-    uint32_t *csc_row = (uint32_t *)malloc(nz*sizeof(uint32_t));
+    uint32_t *csc_row = (uint32_t *)malloc(2*nz*sizeof(uint32_t));
     uint32_t *csc_col = (uint32_t *)malloc((N + 1)*sizeof(uint32_t));
 
-    coo2csc(csc_row, csc_col, I, J, nz, N, isOneBased);
+    coo2csc(csc_row, csc_col, I, J, nz*2, N, isOneBased);
 
-    // printf("print csc_row(must me %d elements):\n", nz);
-    // for (int i = 0; i < nz; i++) {
+    // printf("print csc_row(must me %d elements):\n", 2*nz);
+    // for (int i = 0; i < 2*nz; i++) {
     //   printf("%d :  %d\n", i, csc_row[i]);
     // }
     //
-    // printf("print csc_col(must me %d elements):\n", N);
-    // for (int i = 0; i < N; i++) {
+    // printf("print csc_col(must me %d elements):\n", N+1);
+    // for (int i = 0; i < N + 1; i++) {
     //   printf("%d :  %d\n", i, csc_col[i]);
     // }
-    // printf("\n\n");
+    printf("\n\n");
 
     /* TRIANGLE COUNTER AREA  */
     long elapsed_sec, elapsed_nsec;
@@ -146,22 +203,64 @@ int main(int argc, char *argv[])
     }
 
     printf("\n\n");
+    printf("size of array is %d x %d", N,N);
+    printf("started triangle counting\n");
     clock_gettime(CLOCK_MONOTONIC, &ts_start);
 
-    for (int i = 1; i < N-2; i++) {
 
-      for (int j = return_index(i, csc_col, csc_row); j < csc_col[i + 1]; j++) {
-        for (int k = return_index(j, csc_col, csc_row); k < csc_col[j + 1]; k++) {
-             //arr[i][j] == arr[j][k] == arr[k][i] == 1
+    for (int i = 0; i < N-2; i++) {
 
-             c3[i]++;
-             c3[j]++;
-             c3[k]++;
+      uint32_t start = csc_col[i];
+      uint32_t end = csc_col[i+1];
+
+
+      int *sliced = printsliced(start, end, i, csc_col, csc_row, N);
+      int size = sizeof(sliced)/sizeof(sliced[0]);
+      printf("\n\n");
+
+      printf("currently in loop i: %d", i);
+
+      // check for the zero case
+      if (sliced[0] > i) {
+        foreach(int *j, sliced) {
+
+          uint32_t start2 = csc_col[*j];
+          uint32_t end2 = csc_col[*j+1];
+
+          int *sliced2 = printsliced(start2, end2, *j, csc_col, csc_row, N+1);
+          int size2 = sizeof(sliced2)/sizeof(sliced2[0]);
+          printf("\n\n");
+
+          printf("******currently in loop j: %d", *j);
+          // foreach(int *j, sliced2) {
+          //   printf(" %d, ", *j);
+          // }
+          printf("\n\n");
+
+          foreach (int *k, sliced2) {
+            printf("******--------------currently in loop k: %d", *k);
+            printf("\n\n");
+
+            c3[*k]++;
+
+          }
+
+
+          free(sliced2);
+
         }
       }
+
+
+
+
+      free(sliced);
+
     }
 
     clock_gettime(CLOCK_MONOTONIC, &ts_end);
+    printf("finished triangle counting\n");
+
 
     printf("\n\n");
     elapsed_sec = ts_end.tv_sec - ts_start.tv_sec;
@@ -179,6 +278,10 @@ int main(int argc, char *argv[])
 
 	return 0;
 }
+
+
+
+
 
 
 
