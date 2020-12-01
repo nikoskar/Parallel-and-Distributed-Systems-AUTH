7d60bd4942b4e8a20ef3554a0c649f2e6e14428a
nikoskar <nikoskarabe95@gmail.com>
2020-12-01 03:00:29 +0200

    matrix vector mul

--- v4.c
+++ v4.c
@@ -201,124 +201,122 @@ int main(int argc, char *argv[])
     }
 		printf("\n");
 
-		/*
-		uint32_t *c3 = malloc(N*sizeof(uint32_t));
-		for (uint32_t i = 0; i < N; i++) {
-			c3[i] = 0;
-		}
 
-    for(uint32_t j = 0; j < N - 1; j++) {
+    /*
+     * Create a random vector of dimension Nx1 (N = number of rows/cols of A)
+     * Its cells contain values ranging from 1 to 5 (randomly filled)
+     *
+    */
+    srand(time(NULL));
+    uint32_t *vector = (malloc)(N * sizeof(uint32_t));
+    uint32_t *result = malloc(N * sizeof(uint32_t));
 
+    for(uint32_t i = 0; i < N; i++) {
+      vector[i] = 1 + rand() % 5; // guarantees no zeros
+      result[i] = 0;
+    }
 
-      uint32_t start = csc_col[j];
-      uint32_t end = csc_col[j + 1];
-			uint32_t idx = 0;
-			uint32_t size = end - start;
+    for(uint32_t i = 0; i < N; i++)
+    {
+      uint32_t start = csc_col[i];
+      uint32_t end = csc_col[i + 1];
+      uint32_t sum = 0;
 
-			uint32_t *log = malloc(size*sizeof(uint32_t));
+      //printf("i = %d, start = %d, end = %d\n", i, start, end);
 
-			//printf("\n\n");
-			printf("j: %d has these zeros: ", j);
-
-			for(uint32_t pos = start; pos < end; pos++) {
-				log[idx] = csc_row[pos];
-				printf("%d, ", log[idx]);
-				idx++;
-			}
-			//printf("\n\n");
-
-      for(uint32_t i = start; i < end; i++) {
-
-        uint32_t start1 = csc_col[csc_row[i]];
-        uint32_t end1 = csc_col[csc_row[i] + 1];
-				//printf("in i = %d with start = %d and end = %d", i, start1, end1);
-				//printf("\n ---i = %d", csc_row[i]);
-        for(uint32_t k = start1; k < end1; k++) {
-					//printf("\n ------k = %d (j = %d, i = %d)", csc_row[k], j, csc_row[i]);
-
-					if(csc_row[k]) {
-						for(uint32_t x = 0; x < size; x++)
-						{
-							if(csc_row[k] == log[x]) {
-								printf("\n ------k = %d (j = %d, i = %d)", csc_row[k], j, csc_row[i]);
-								c3[csc_row[k]]++;
-								c3[j]++;
-								c3[csc_row[i]]++;
-								printf("\ntriangle found\n");
-							}
-						}
-					}
-					// for(uint32_t x = 0; x < size; x++)
-					// {
-					// 	if(csc_row[k] == log[x]) {
-					// 		c3[csc_row[k]]++;
-					// 		c3[csc_row[j]]++;
-					// 		c3[csc_row[i]]++;
-					// 		//printf("triangle found\n");
-					// 	}
-					// }
-        }
-				printf("\n");
+      for(uint32_t j = start; j < end; j++) {
+        sum = sum + vector[csc_row[j]];
       }
-			printf("\n");
-			free(log);
+      result[i] = sum;
+      //printf(" the total sum is %d\n: ", sum);
 
     }
-		for (uint32_t i = 0; i < N; i++) {
-			printf("node %d has %d triangles\n", i, c3[i]);
-		}
-		free(c3);
-		*/
 
+    printf("\nthe vector is:\n");
+    for(int i = 0; i < N; i ++) {
+      printf("%d, ", vector[i]);
+    }
+    printf("\nthe result is:\n");
+    for(int i = 0; i < N; i ++) {
+      printf("%d, ", result[i]);
+    }
+    printf("\n");
 
-	return 0;
+    free(result);
+    free(vector);
+    free(csc_col);
+    free(csc_row);
+    free(I);
+    free(J);
+  	return 0;
 }
 
 
-
 /*
+uint32_t *c3 = malloc(N*sizeof(uint32_t));
+for (uint32_t i = 0; i < N; i++) {
+  c3[i] = 0;
+}
+
+for(uint32_t j = 0; j < N - 1; j++) {
 
 
+  uint32_t start = csc_col[j];
+  uint32_t end = csc_col[j + 1];
+  uint32_t idx = 0;
+  uint32_t size = end - start;
 
+  uint32_t *log = malloc(size*sizeof(uint32_t));
 
+  //printf("\n\n");
+  printf("j: %d has these zeros: ", j);
 
+  for(uint32_t pos = start; pos < end; pos++) {
+    log[idx] = csc_row[pos];
+    printf("%d, ", log[idx]);
+    idx++;
+  }
+  //printf("\n\n");
 
+  for(uint32_t i = start; i < end; i++) {
 
+    uint32_t start1 = csc_col[csc_row[i]];
+    uint32_t end1 = csc_col[csc_row[i] + 1];
+    //printf("in i = %d with start = %d and end = %d", i, start1, end1);
+    //printf("\n ---i = %d", csc_row[i]);
+    for(uint32_t k = start1; k < end1; k++) {
+      //printf("\n ------k = %d (j = %d, i = %d)", csc_row[k], j, csc_row[i]);
 
+      if(csc_row[k]) {
+        for(uint32_t x = 0; x < size; x++)
+        {
+          if(csc_row[k] == log[x]) {
+            printf("\n ------k = %d (j = %d, i = %d)", csc_row[k], j, csc_row[i]);
+            c3[csc_row[k]]++;
+            c3[j]++;
+            c3[csc_row[i]]++;
+            printf("\ntriangle found\n");
+          }
+        }
+      }
+      // for(uint32_t x = 0; x < size; x++)
+      // {
+      // 	if(csc_row[k] == log[x]) {
+      // 		c3[csc_row[k]]++;
+      // 		c3[csc_row[j]]++;
+      // 		c3[csc_row[i]]++;
+      // 		//printf("triangle found\n");
+      // 	}
+      // }
+    }
+    printf("\n");
+  }
+  printf("\n");
+  free(log);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
+for (uint32_t i = 0; i < N; i++) {
+  printf("node %d has %d triangles\n", i, c3[i]);
+}
+free(c3);
 */
